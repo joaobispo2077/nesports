88ed4b51246c18184c0207bd95d24c96e2b2a62c
João Bispo <joaobispo2077@gmail.com>
2022-09-17 19:50:47 -0300

    feat: add games table

---
+++ server/prisma/migrations/20220917224632_create_table_games/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Game" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "bannerUrl" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
